30c27c80c8dff0a9e1c1b740784a2944ee2c67f5
mekdesmekdesa.hub <mekdesmekdesa@gmail.com>
2026-07-24 14:20:45 +0300

    used map method to refractor

--- react-project-102/components/entry.jsx
+++ react-project-102/components/entry.jsx
@@ -1,19 +1,19 @@
-function Entry() {
+function Entry(props    ) {
   return (
     <div className="entry">
       <div className="entry-image-container">
-       <img src="src/assets/Japan.png" alt="japan" className="japan-image"/>
+       <img src={props.img.src} alt={props.img.alt} className="japan-image"/>
       </div>
       <div className="entry-details">
       <div className="entry-details-1">
         <img src="src/assets/location.svg" alt="Location" className="location-icon"/>
-        <p className="location">Japan</p>
-        <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu" className="maps-link">View on Google Maps</a>
+        <p className="location">{props.country}</p>
+        <a href={props.googleMapsLink} className="maps-link">View on Google Maps</a>
       </div>
       <div className="entry-details-2">
-        <p className="title">Mount Fuji</p>  
-        <h3 className="dates">12 Jan, 2021 - 24 Jan, 2021</h3>
-        <p className="description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+        <p className="title">{props.title}</p>  
+        <h3 className="dates">{props.dates}</h3>
+        <p className="description">{props.text}</p>
       </div>
       </div>
     </div>
--- react-project-102/src/App.jsx
+++ react-project-102/src/App.jsx
@@ -5,15 +5,28 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Header from '../components/header.jsx'
 import Entry from '../components/entry.jsx'
+import data from './assets/data.js' 
 
 function App() {
-
+  const dataDetails= data.map((detail)=>{
+    return(
+      <Entry 
+        key={detail.id}
+        img={detail.img}
+        title={detail.title}
+        country={detail.country}
+        googleMapsLink={detail.googleMapsLink}
+        dates={detail.dates}
+        text={detail.text}
+      />
+    )
+  })
 
   return (
     
       <div className='whole-body'>
         < Header />
-        <Entry />
+        {dataDetails}
       </div>
   )
 }
